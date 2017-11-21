5ae0a1ba0ec80ed3c43abc60bba4b1f93c1f8803
mabdullahmunir <33483161+mabdullahmunir@users.noreply.github.com>
2017-11-21 20:23:06 +0700

    delete define

--- mesincmd.h
+++ mesincmd.h
@@ -17,17 +17,6 @@ typedef struct {
 extern Kata Cmd;
 extern char CCmd;
 
-#define move (Kata){"MOVE", 4}
-#define undo (Kata){"UNDO", 4}
-#define change_unit (Kata){"CHANGE_UNIT", 11}
-#define recruit (Kata){"RECRUIT", 7}
-#define attack (Kata){"ATTACK", 6}
-#define map (Kata){"MAP", 3}
-#define info (Kata){"INFO", 4}
-#define end_turn (Kata){"END_TURN", 8}
-#define save (Kata){"SAVE", 4}
-#define exit (Kata){"EXIT", 4}
-
 void ADVC();
 void get_cmd();
 boolean cmpkata(Kata K1, Kata K2);
